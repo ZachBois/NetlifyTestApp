34d987b8761a06b71111700ee6e4447a421d2354
Zach Bois <zachboiss1@gmail.com>
2022-05-31 13:24:51 -0500

    Changed Function

--- netlify/functions/script.js
+++ netlify/functions/script.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+exports.handler = async function (event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World" }),
+    };
+  }
